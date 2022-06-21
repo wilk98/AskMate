c03d82025bcee37ead3fd0d04011bf3ad8d73eab
Galyna <77695117+GalkaNH@users.noreply.github.com>
2022-06-21 14:08:46 +0200

    add

--- server.py
+++ server.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+from forms import PostForm
 
 
 app = Flask(__name__)
 
+@app.route('/create')
+def create_post():
+    form = PostForm()
+    return render_template('/create_post.html', form=form)
 
 @app.route("/")
 def index():
@@ -12,6 +17,11 @@ def index():
 @app.route("/list")
 def list_page():
     return render_template('list.html')
+
+
+@app.route('/question')
+def questions_page():
+    return render_template('question.html')
 
 
 if __name__ == "__main__":
