7c7fc7183e7a758a01999b141468cdd4d6835e7c
Galyna <77695117+GalkaNH@users.noreply.github.com>
2022-06-21 08:35:33 +0200

    add templates, static, venv

--- server.py
+++ server.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('index.html')
+
+
+@app.route("/list")
+def list_page():
+    return render_template('list.html')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
