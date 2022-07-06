a86206f22445a73d3cbc4123413c2c528eb24ba5
MKluczniak <61352124+MKluczniak@users.noreply.github.com>
2022-07-06 09:30:06 +0200

    dasda

--- data_manager.py
+++ data_manager.py
@@ -1,16 +1,13 @@
-<<<<<<< HEAD
+
 import db_common
 
-QUESTION_HEADER = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
 
 
-=======
 from typing import List, Dict
 from psycopg2 import sql
 from psycopg2.extras import RealDictCursor
 
 import db_common
->>>>>>> Maciek
 
 @db_common.connection_handler
 def read_questions(cursor):
@@ -20,10 +17,18 @@ def read_questions(cursor):
         ORDER BY id"""
     cursor.execute(query)
     return cursor.fetchall()
-<<<<<<< HEAD
-=======
+
 
 @db_common.connection_handler
-def get_question(cursor)
-    query
->>>>>>> Maciek
+def search_question(cursor, ):
+    query = """
+        SELECT *
+        FROM question
+        ORDER BY id"""
+    cursor.execute(query)
+    return cursor.fetchall()
+
+
+
+
+
