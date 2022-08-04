b61bef79582ba274536d03ea9d57f244b8e695fa
MKluczniak <61352124+MKluczniak@users.noreply.github.com>
2022-08-05 00:50:56 +0200

    font size changing

---
+++ static/js/fontSize.js
@@ -0,0 +1,11 @@
+let body = document.getElementsByTagName("body")[0];
+
+let pTags = document.getElementsByTagName('p');
+
+let up = document.getElementById("increase")
+let down = document.getElementById("decrease")
+
+up.onclick = function () {
+    body.style.fontSize = "30px";
+    
+};
