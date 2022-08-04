b4419700abc48f780b7eb100b751b86c81cae44b
MKluczniak <61352124+MKluczniak@users.noreply.github.com>
2022-08-05 01:01:07 +0200

    fontsiz.js added

--- static/js/fontSize.js
+++ static/js/fontSize.js
@@ -1,11 +1,22 @@
 let body = document.getElementsByTagName("body")[0];
 
 let pTags = document.getElementsByTagName('p');
+let decreaseSign = document.getElementById('decrease');
+let increaseSign = document.getElementById('increase');
 
-let up = document.getElementById("increase")
-let down = document.getElementById("decrease")
 
-up.onclick = function () {
-    body.style.fontSize = "30px";
-    
+decreaseSign.onclick = function () {
+    body.style.fontSize = "10px"
+    return false
 };
+
+increaseSign.onclick = function () {
+    body.style.fontSize = "50px"
+    return false
+};
+
+
+
+
+
+console.log("dupa2")
