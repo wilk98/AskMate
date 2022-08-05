bba40c24e5834d2bfd4d10f029768f3fb284e819
= <=>
2022-08-05 10:32:27 +0200

    final version

--- static/js/bonusQuestions.js
+++ static/js/bonusQuestions.js
@@ -40,13 +40,49 @@ function getFilteredItems(items, filterValue) {
 }
 
 function toggleTheme() {
+
     console.log("toggle theme")
 }
+let increaseFont = document.getElementById("increase-font-button")
+let decreaseFont = document.getElementById("decrease-font-button")
+let body = document.getElementsByTagName("body")[0];
+let xxx = document.getElementById("doNotModifyThisId_QuestionsTableHeader");
+let xxx2 = document.getElementById("doNotModifyThisId_QuestionsFilter");
+let xxx3 = document.getElementById("doNotModifyThisId_QuestionsTableBody");
+let xxx4 = document.getElementsByTagName("button");
 
-function increaseFont() {
-    console.log("increaseFont")
-}
+console.log(xxx)
 
-function decreaseFont() {
+let ccc = 16
+
+
+increaseFont.onclick = function increaseFont() {
+
+    if (ccc < 25) {
+        ccc = ++ccc;
+        document.body.style.fontSize = ccc + "px";   //bez zmiennej na body
+        xxx.style.fontSize = ccc + "px";
+        xxx2.style.fontSize = ccc + "px";
+        xxx3.style.fontSize = ccc + "px";
+        xxx3.style.fontSize = ccc + "px";
+        Array.from(xxx4).forEach(btn => {
+            btn.style.fontSize = ccc + 'px';
+        });
+        // let currentFontSize = xxx3.style.fontSize || ccc++"16px"
+        console.log("increaseFont")
+        console.log(ccc)
+    }}
+
+
+decreaseFont.onclick = function decreaseFont() {
+        if (ccc > 3){
+    ccc=--ccc;
+    document.body.style.fontSize = ccc+"px";   //bez zmiennej na body
+    xxx.style.fontSize = ccc+"px";
+    xxx2.style.fontSize = ccc+"px";
+    xxx3.style.fontSize = ccc+"px";
+    xxx3.style.fontSize = ccc+"px";
+    Array.from(xxx4).forEach( btn => { btn.style.fontSize = ccc + 'px'; } );
+
     console.log("decreaseFont")
-}
+}}
