30eeb04595da949fc2c1cb9d6883d33ae08057a3
Galyna <77695117+GalkaNH@users.noreply.github.com>
2022-08-05 07:19:21 +0200

    dodano styli

--- data_manager.py
+++ data_manager.py
@@ -19,7 +19,7 @@ def get_question(cursor, question_id):
             FROM question
             WHERE id = %s"""
     cursor.execute(query, (question_id,))
-    return cursor.fetchall()
+    return cursor.fetchone()
 
 
 @db_common.connection_handler
@@ -37,7 +37,7 @@ def get_answer(cursor, answer_id):
         FROM answer
         WHERE id = %s"""
     cursor.execute(query, (answer_id,))
-    return cursor.fetchall()
+    return cursor.fetchone()
 
 
 @db_common.connection_handler
@@ -59,6 +59,15 @@ def get_comment_question(cursor, question_id):
 
 
 @db_common.connection_handler
+def get_comment(cursor, comment_id):
+    query = """SELECT *
+        FROM comment
+        WHERE id = %s"""
+    cursor.execute(query, (comment_id,))
+    return cursor.fetchone()
+
+
+@db_common.connection_handler
 def get_tag_id(cursor, question_id):
     query = """SELECT tag_id
         FROM question_tag
@@ -77,55 +86,69 @@ def get_tag(cursor, tag_id):
 
 
 @db_common.connection_handler
+def get_users(cursor):
+    query = """SELECT * 
+            FROM member
+            ORDER BY member_id"""
+    cursor.execute(query)
+    return cursor.fetchall()
+
+
+@db_common.connection_handler
 def post_question(cursor, question_detail):
     cursor.execute("""INSERT INTO question
-        (submission_time, view_number, vote_number, title, message, image)
-        VALUES (%(s_t)s, %(v_w)s, %(v_e)s, %(t_e)s, %(m_e)s, %(i_e)s)""",
+        (submission_time, view_number, vote_number, title, message, image, member_id)
+        VALUES (%(s_t)s, %(v_w)s, %(v_e)s, %(t_e)s, %(m_e)s, %(i_e)s, %(m_i)s)""",
                    {'s_t': question_detail['submission_time'],
                     'v_w': question_detail['view_number'],
                     'v_e': question_detail['vote_number'],
                     't_e': question_detail['title'],
                     'm_e': question_detail['message'],
-                    'i_e': question_detail['image']
+                    'i_e': question_detail['image'],
+                    'm_i': question_detail['member_id']
                     })
 
 
 @db_common.connection_handler
 def post_answer(cursor, answer):
     cursor.execute("""INSERT INTO answer\
-    (submission_time, vote_number, question_id, message, image)
-    VALUES (%(s_t)s, %(v_n)s, %(q_i)s, %(m_e)s, %(i_e)s)""",
-                   {'s_t': answer['submission_time'],
-                    'v_n': answer['vote_number'],
+    (submission_time, vote_number, question_id, message, image, member_id)
+    VALUES (%(s_t)s, %(v_n)s, %(q_i)s, %(m_e)s, %(i_e)s, %(m_i)s)""",
+                   {
+                       's_t': answer['submission_time'],
+                       'v_n': answer['vote_number'],
                        'q_i': answer['question_id'],
                        'm_e': answer['message'],
-                       'i_e': answer['image']
-                    })
+                       'i_e': answer['image'],
+                       'm_i': answer['member_id']
+                   })
 
 
 @db_common.connection_handler
 def post_comment_answer(cursor, comment):
     cursor.execute("""INSERT INTO comment
-        (answer_id, message, submission_time, edited_count)
-        VALUES ( %(a_i)s, %(m_e)s, %(s_t)s, %(e_c)s)""",
+        (answer_id, message, submission_time, edited_count, member_id)
+        VALUES ( %(a_i)s, %(m_e)s, %(s_t)s, %(e_c)s, %(m_i)s)""",
                    {
                        'a_i': comment['answer_id'],
                        'm_e': comment['message'],
                        's_t': comment['submission_time'],
                        'e_c': comment['edited_count'],
+                       'm_i': comment['member_id']
                    })
 
 
 @db_common.connection_handler
 def post_comment_question(cursor, comment):
     cursor.execute("""INSERT INTO comment
-        (question_id, message, submission_time, edited_count)
-        VALUES ( %(q_i)s, %(m_e)s, %(s_t)s, %(e_c)s)""",
+        (question_id, message, submission_time, edited_count, member_id)
+        VALUES ( %(q_i)s, %(m_e)s, %(s_t)s, %(e_c)s, %(m_i)s)""",
                    {
                        'q_i': comment['question_id'],
                        'm_e': comment['message'],
                        's_t': comment['submission_time'],
                        'e_c': comment['edited_count'],
+                       'm_i': comment['member_id']
                    })
 
 
@@ -192,7 +215,8 @@ def vote_answer_down(cursor, answer_id):
 @db_common.connection_handler
 def edit_question(cursor, question_to_edit):
     query = f"UPDATE question\
-        SET title = '{question_to_edit['title']}', message = '{question_to_edit['message']}', image = '{question_to_edit['image']}'\
+        SET title = '{question_to_edit['title']}', " \
+            f"message = '{question_to_edit['message']}', image = '{question_to_edit['image']}'\
         WHERE id = '{question_to_edit['id']}';"
     return cursor.execute(query)
 
@@ -202,6 +226,14 @@ def edit_answer(cursor, answer_to_edit):
     query = f"UPDATE answer\
         SET message = '{answer_to_edit['message']}', image = '{answer_to_edit['image']}'\
         WHERE id = '{answer_to_edit['id']}';"
+    return cursor.execute(query)
+
+
+@db_common.connection_handler
+def edit_comment(cursor, comment_to_edit):
+    query = f"UPDATE comment\
+        SET message = '{comment_to_edit['message']}'\
+        WHERE id = '{comment_to_edit['id']}';"
     return cursor.execute(query)
 
 
@@ -226,7 +258,7 @@ def get_question_by_column(cursor, column_select, order):
             SELECT id, submission_time, view_number, vote_number,
                     title, message, image
             FROM question
-            ORDER BY """
+            ORDER BY id """
     query += column_select
     if order and order in ['asc', 'desc']:
         query += " " + order
@@ -236,22 +268,127 @@ def get_question_by_column(cursor, column_select, order):
 
 @db_common.connection_handler
 def get_search(cursor, q):
-    query = f"SELECT title, message, submission_time\
-        FROM question\
-        WHERE title = '{q}'\
-        ORDER BY submission_time"
-
-    cursor.execute(query)
+    cursor.execute("""SELECT *
+                      FROM
+                      question
+                      WHERE title ILIKE %(q1)s""", {'q1': f"%{q}%"})
     return cursor.fetchall()
 
 
 @db_common.connection_handler
 def addUser(cursor, new_user):
     print(new_user)
-    cursor.execute("""INSERT INTO member(user_name, password, registration_date)
-                   VALUES (%(e_m)s, %(p_s)s, %(t_m)s )""",
+    cursor.execute("""INSERT INTO member(user_name, password, registration_date, number_of_questions, number_of_answers, number_of_comments, reputation)
+                   VALUES (%(e_m)s, %(p_s)s, %(t_m)s, %(n_q)s, %(n_a)s, %(n_c)s, %(r_n)s )""",
                    {
                        'e_m': new_user['user_name'],
                        'p_s': new_user['password'],
-                       't_m': new_user['registration_date']
+                       't_m': new_user['registration_date'],
+                       'n_q': new_user['number_of_questions'],
+                       'n_a': new_user['number_of_answers'],
+                       'n_c': new_user['number_of_comments'],
+                       'r_n': new_user['reputation'],
                    })
+
+
+@db_common.connection_handler
+def get_user_password(cursor, user_name):
+    cursor.execute("""SELECT member_id, password FROM member
+                        WHERE user_name = %(username)s""",
+                   {
+                       'username': user_name,
+                   })
+    return cursor.fetchone()
+
+
+@db_common.connection_handler
+def get_user_data(cursor, user_id):
+    cursor.execute("""SELECT member_id, user_name, registration_date, number_of_questions, 
+                        number_of_answers, number_of_comments, reputation 
+                        FROM member
+                        WHERE member_id = %(user_id)s""",
+                   {
+                       'user_id': user_id,
+                   })
+    return cursor.fetchone()
+
+
+@db_common.connection_handler
+def get_user_questions(cursor, user_id):
+    cursor.execute("""SELECT id, title FROM question
+    WHERE member_id = %(user_id)s""",
+                   {'user_id': user_id,
+                    })
+    return cursor.fetchall()
+
+
+@db_common.connection_handler
+def get_user_answers(cursor, user_id):
+    cursor.execute("""SELECT id, message FROM answer
+    WHERE member_id = %(user_id)s""",
+                   {'user_id': user_id,
+                    })
+    return cursor.fetchall()
+
+
+@db_common.connection_handler
+def get_user_comments(cursor, user_id):
+    cursor.execute("""SELECT id, message FROM comment
+    WHERE member_id = %(user_id)s""",
+                   {'user_id': user_id,
+                    })
+    return cursor.fetchall()
+
+
+@db_common.connection_handler
+def add_number_question(cursor, user_id):
+    query = """UPDATE member
+            SET number_of_questions = number_of_questions + 1
+            WHERE member_id = %s;
+            """
+    return cursor.execute(query, (user_id,))
+
+
+@db_common.connection_handler
+def add_number_answer(cursor, user_id):
+    query = """UPDATE member
+            SET number_of_answers = number_of_answers + 1
+            WHERE member_id = %s;
+            """
+    return cursor.execute(query, (user_id,))
+
+
+@db_common.connection_handler
+def add_number_comment(cursor, user_id):
+    query = """UPDATE member
+            SET number_of_comments = number_of_comments + 1
+            WHERE member_id = %s;
+            """
+    return cursor.execute(query, (user_id,))
+
+
+@db_common.connection_handler
+def delete_number_question(cursor, user_id):
+    query = """UPDATE member
+            SET number_of_questions = number_of_questions - 1
+            WHERE member_id = %s;
+            """
+    return cursor.execute(query, (user_id,))
+
+
+@db_common.connection_handler
+def delete_number_answer(cursor, user_id):
+    query = """UPDATE member
+            SET number_of_answers = number_of_answers - 1
+            WHERE member_id = %s;
+            """
+    return cursor.execute(query, (user_id,))
+
+
+@db_common.connection_handler
+def delete_number_comment(cursor, user_id):
+    query = """UPDATE member
+            SET number_of_comments = number_of_comments - 1
+            WHERE member_id = %s;
+            """
+    return cursor.execute(query, (user_id,))
--- server.py
+++ server.py
@@ -3,21 +3,33 @@ from nturl2path import url2pathname
 from turtle import title
 from flask import Flask, flash, render_template, url_for, session, escape
 from bonus_questions import SAMPLE_QUESTIONS
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session
 import connection
 import time
 from datetime import datetime
 import data_manager
 from werkzeug.security import generate_password_hash, check_password_hash
-
-
-class User:
-    def __init__(self, id, username, password):
-        pass
-
+from functools import wraps
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'ghbdtn93vbh65bdctv407yfv'
+app.secret_key = 'ghbdtn93vbh65bdctv407yfv'
+
+
+def get_logged_user():
+    if 'user_name' in session:
+        return {'user_name': session['user_name'], 'member_id': session['member_id']}
+    else:
+        return None
+
+
+def login_required(f):
+    @wraps(f)
+    def decorate_func(*args, **kwargs):
+        if 'member_id' in session:
+            return f(*args, **kwargs)
+        else:
+            return redirect("/login")
+    return decorate_func
 
 
 @app.route("/bonus-questions")
@@ -27,22 +39,50 @@ def main():
 
 @app.route("/")
 @app.route("/index")
-def hello():
-    if 'user_name' in session:
-        username = escape(session['user_name'])
+def index():
+    if 'member_id' in session:
         print(session)
-        return render_template('index.html', username=username)
+        print(session['member_id'])
+        return render_template('index.html', logged_user=get_logged_user())
     return render_template('index.html')
 
 
 @app.route("/login", methods=["POST", 'GET'])
 def login():
+    user = {}
+    invalid_credentials = False
     if request.method == "POST":
-        hash = generate_password_hash(request.form['psw'])
-        session['user_name'] = request.form['email']
-        session['password'] = hash
+        user_name = request.form['email']
+        password = request.form['psw']
+        user_data = data_manager.get_user_password(user_name)
+        if user_data and check_password_hash(user_data['password'], password):
+            session['member_id'] = user_data['member_id']
+            session['user_name'] = user_name
+            return redirect(url_for('index'))
+        else:
+            invalid_credentials = True
+            print("bad login")
 
-    return render_template('login.html',  title="authorization")
+    return render_template('login.html',  title="authorization", invalid_credentials=invalid_credentials)
+
+
+@app.route('/user/<user_id>')
+@login_required
+def get_user_data(user_id):
+    user = data_manager.get_user_data(user_id)
+    questions = data_manager.get_user_questions(user_id)
+    answers = data_manager.get_user_answers(user_id)
+    comments = data_manager.get_user_comments(user_id)
+    return render_template('user.html', user=user,
+                           logged_user=get_logged_user(),
+                           questions=questions, answers=answers, comments=comments)
+
+
+@app.route("/logout")
+def logout():
+    session.pop('member_id', None)
+    session.pop('user_name', None)
+    return redirect(url_for("login"))
 
 
 @app.route("/register", methods=["POST", 'GET'])
@@ -57,34 +97,50 @@ def register():
             new_user['password'] = hash
             new_user['registration_date'] = datetime.fromtimestamp(
                 ts_epoch).strftime('%Y-%m-%d %H:%M:%S')
+            new_user['number_of_questions'] = 0
+            new_user['number_of_answers'] = 0
+            new_user['number_of_comments'] = 0
+            new_user['reputation'] = 0
             data_manager.addUser(new_user)
             if new_user:
-                flash("You have successfully registered!", "sussess")
+                flash("You have successfully registered!", category="success")
                 return redirect(url_for('login'))
             else:
-                flash("Error adding to database", "error")
+                flash("Error adding to database", category="error")
         else:
-            flash("The form contains errors", "error")
+            flash("The form contains errors", category="error")
 
     return render_template('register.html',  title="register")
+
+
+@app.route("/users")
+@login_required
+def show_users():
+    members = data_manager.get_users()
+    return render_template('users.html', member=members, logged_user=get_logged_user())
 
 
 @app.route("/list")
 def show_questions():
     order_by_column = request.args.get('column-select')
     direction = request.args.get('order')
-    # get_scan = request.args.get('scan')
+    get_scan = request.args.get('scan')
     if order_by_column:
         questions = data_manager.get_question_by_column(
             order_by_column, direction)
-    # if get_scan:
-    #     questions = data_manager.get_search(get_scan)
+    elif get_scan:
+        questions = data_manager.get_search(get_scan)
     else:
         questions = data_manager.read_questions()
-    return render_template('list.html', list=questions, column_select=order_by_column, order=direction)
+    return render_template('list.html',
+                           list=questions,
+                           column_select=order_by_column,
+                           order=direction,
+                           logged_user=get_logged_user())
 
 
 @app.route('/question', methods=["POST", "GET"])
+@login_required
 def add_question():
     ts_epoch = (int(time.time()))
     question_to_add = {}
@@ -96,14 +152,19 @@ def add_question():
         question_to_add['title'] = request.form['title']
         question_to_add['message'] = request.form['message']
         question_to_add['image'] = request.form['image']
+        question_to_add['member_id'] = session['member_id']
+        # user_id = session['userid']
         data_manager.post_question(question_to_add)
+        data_manager.add_number_question(session['member_id'])
         return redirect('/list')
     else:
         return render_template('question.html', title=question_to_add.get('title'),
-                               message=question_to_add.get('message'))
+                               message=question_to_add.get('message'),
+                               logged_user=get_logged_user())
 
 
 @app.route('/question/<question_id>/new_answer', methods=['POST', 'GET'])
+@login_required
 def add_new_answer(question_id):
     ts_epoch = (int(time.time()))
     answer_to_post = {}
@@ -114,14 +175,18 @@ def add_new_answer(question_id):
         answer_to_post['question_id'] = question_id
         answer_to_post['message'] = request.form['message']
         answer_to_post['image'] = request.form['image']
+        answer_to_post['member_id'] = session['member_id']
         data_manager.post_answer(answer_to_post)
+        data_manager.add_number_answer(session['member_id'])
         return redirect('/list')
     return render_template('answer.html',
                            question_id=question_id,
-                           message='')
+                           message='',
+                           logged_user=get_logged_user())
 
 
 @app.route('/question/<question_id>/new-comment', methods=['POST', 'GET'])
+@login_required
 def add_comment_question(question_id):
     ts_epoch = (int(time.time()))
     comment_to_post = {}
@@ -131,13 +196,16 @@ def add_comment_question(question_id):
         comment_to_post['message'] = request.form['comment']
         comment_to_post['edited_count'] = 0
         comment_to_post['question_id'] = question_id
+        comment_to_post['member_id'] = session['member_id']
         data_manager.post_comment_question(comment_to_post)
-
+        data_manager.add_number_comment(session['member_id'])
         return redirect('/list')
-    return render_template('comment_question.html', question_id=question_id)
+    return render_template('comment_question.html', question_id=question_id,
+                           logged_user=get_logged_user())
 
 
 @app.route('/answer/<answer_id>/new-comment', methods=['POST', 'GET'])
+@login_required
 def add_comment_answer(answer_id):
     ts_epoch = (int(time.time()))
     comment_to_post = {}
@@ -147,10 +215,12 @@ def add_comment_answer(answer_id):
         comment_to_post['message'] = request.form['comment']
         comment_to_post['edited_count'] = 0
         comment_to_post['answer_id'] = answer_id
+        comment_to_post['member_id'] = session['member_id']
         data_manager.post_comment_answer(comment_to_post)
-
+        data_manager.add_number_comment(session['member_id'])
         return redirect('/list')
-    return render_template('comment_answer.html', answer_id=answer_id)
+    return render_template('comment_answer.html', answer_id=answer_id,
+                           logged_user=get_logged_user())
 
 
 # @app.route("/question/<question_id>")
@@ -179,61 +249,75 @@ def display_question(question_id):
             all_tags.append(i)
         else:
             all_tags.append(i)
-    return render_template('question_to_show.html', question=question_to_show[0], answers=answers_to_show, comment=comment_to_show, tag=all_tags)
+    return render_template('question_to_show.html', question=question_to_show,
+                           answers=answers_to_show, comment=comment_to_show, tag=all_tags,
+                           logged_user=get_logged_user())
 
 
 @app.route("/answer/<answer_id>")
 def display_answer(answer_id):
     answer_to_show = data_manager.get_answer(answer_id)
     comment_to_show = data_manager.get_comment_answer(answer_id)
-    return render_template('answer_to_show.html', comment=comment_to_show, answer=answer_to_show[0])
+    return render_template('answer_to_show.html', comment=comment_to_show, answer=answer_to_show,
+                           logged_user=get_logged_user())
 
 
 @app.route('/question/<question_id>/delete')
+@login_required
 def delete_question(question_id):
     print(question_id)
     data_manager.delete_answers(question_id)
     data_manager.delete_question(question_id)
+    data_manager.delete_number_question(session['member_id'])
     return redirect('/list')
 
 
 @app.route('/answer/<answer_id>/delete')
+@login_required
 def delete_answer(answer_id):
     data_manager.delete_answer(answer_id)
+    data_manager.delete_number_answer(session['member_id'])
     return redirect('/list')
 
 
 @app.route('/comments/<comment_id>/delete')
+@login_required
 def delete_comment(comment_id):
     data_manager.delete_comment(comment_id)
+    data_manager.delete_number_comment(session['member_id'])
     return redirect('/list')
 
 
 @app.route('/question/<question_id>/vote-up')
+@login_required
 def que_vote_up(question_id):
     data_manager.vote_question_up(question_id)
     return redirect('/list')
 
 
 @app.route('/question/<question_id>/vote-down')
+@login_required
 def que_vote_down(question_id):
     data_manager.vote_question_down(question_id)
     return redirect('/list')
 
 
 @app.route('/answer/<answer_id>/vote-up')
+@login_required
 def ans_vote_up(answer_id):
     data_manager.vote_answer_up(answer_id)
     return redirect('/list')
 
 
 @app.route('/answer/<answer_id>/vote-down')
+@login_required
 def ans_vote_down(answer_id):
     data_manager.vote_answer_down(answer_id)
     return redirect('/list')
 
 
 @app.route('/question/<question_id>/edit', methods=["POST", 'GET'])
+@login_required
 def edit_question(question_id):
     question_to_edit = {}
     if request.method == "POST":
@@ -244,10 +328,16 @@ def edit_question(question_id):
         data_manager.edit_question(question_to_edit)
         return redirect(f'/question/{question_id}')
     else:
-        return render_template('edit_question.html', question_id=question_id)
+        question_to_edit = data_manager.get_question(question_id)
+        title = question_to_edit['title']
+        question = question_to_edit['message']
+        return render_template('edit_question.html', question_id=question_id,
+                               title=title, message=question,
+                               logged_user=get_logged_user())
 
 
 @app.route('/answer/<answer_id>/edit', methods=["POST", 'GET'])
+@login_required
 def edit_answer(answer_id):
     answer_to_edit = {}
     if request.method == "POST":
@@ -257,28 +347,49 @@ def edit_answer(answer_id):
         data_manager.edit_answer(answer_to_edit)
         return redirect(f'/answer/{answer_id}')
     else:
-        return render_template('edit_answer.html', answer_id=answer_id)
+        return render_template('edit_answer.html', answer_id=answer_id,
+                               logged_user=get_logged_user())
+
+
+@app.route('/comments/<comment_id>/edit', methods=["POST", 'GET'])
+@login_required
+def edit_comment(comment_id):
+    comment_to_edit = {}
+    if request.method == "POST":
+        comment_to_edit['id'] = comment_id
+        comment_to_edit['message'] = request.form['message']
+        data_manager.edit_comment(comment_to_edit)
+        return redirect('/list')
+    else:
+        comment_to_edit = data_manager.get_comment(comment_id)
+        comment = comment_to_edit['message']
+        return render_template('comment.html', comment_id=comment_id,
+                               message=comment,
+                               logged_user=get_logged_user())
 
 
 @app.route("/question/<question_id>/new-tag", methods=["POST", 'GET'])
+@login_required
 def add_tag(question_id):
     if request.method == "POST":
         new_tag = request.form['tag']
         data_manager.add_tag(new_tag, question_id)
         return redirect(f'/list')
     else:
-        return render_template('tag.html', question_id=question_id)
+        return render_template('tag.html', question_id=question_id,
+                               logged_user=get_logged_user())
 
 
 @app.route("/team")
 def team_site():
-    return render_template('team.html')
+    return render_template('team.html', logged_user=get_logged_user())
 
 
 @app.route("/most_popular")
 def most_popular_site():
     top_questions = connection.top_questions()
-    return render_template('most_popular.html', most_popular=top_questions)
+    return render_template('most_popular.html', most_popular=top_questions,
+                           logged_user=get_logged_user())
 
 
 if __name__ == "__main__":
